c8ea9967c4d6e7cb04a2c2405531e172e5e9ecd1
lucifer2661 <aditya.hackerface@gmail.com>
2026-03-26 11:50:40 +0530

    js oops practice

--- script.js
+++ script.js
@@ -1,80 +1,37 @@
-let toastCount = 0;
-
-function createToaster(config) {
-  return function (str) {
-
-    const div = document.createElement("div");
-    div.innerText = str;
-
-    // base styles
-    div.classList.add(
-      "inline-block",
-      "px-4",
-      "py-2",
-      "rounded-md",
-      "shadow-md",
-      "fixed",
-      "transition-opacity",
-      "duration-300",
-      "opacity-0"
-    );
-
-    // theme
-    if (config.theme === "dark") {
-      div.classList.add("bg-gray-800", "text-white");
-    } else {
-      div.classList.add("bg-white", "text-gray-800");
-    }
-
-    // horizontal position
-    if (config.positionX === "right") {
-      div.classList.add("right-4");
-    } else {
-      div.classList.add("left-4");
-    }
-
-    // stacking logic
-    toastCount++;
-
-    const spacing = 60; // distance between toasts
-    const offset = 20 + (toastCount - 1) * spacing;
-
-    if (config.positionY === "top") {
-      div.style.top = offset + "px";
-    } else {
-      div.style.bottom = offset + "px";
-    }
-
-    document.body.appendChild(div);
-
-    // fade in
-    setTimeout(() => {
-      div.classList.remove("opacity-0");
-      div.classList.add("opacity-100");
-    }, 100);
-
-    // fade out + remove
-    setTimeout(() => {
-      div.classList.remove("opacity-100");
-      div.classList.add("opacity-0");
-
-      setTimeout(() => {
-        div.remove();
-        toastCount--; // reduce count when removed
-      }, 300);
-    }, config.duration * 1000);
-  };
+function createbiscuits(name, price, qty, company, category){
+  this.name=name;
+  this.price=price;
+  this.qty=qty;
+  this.company=company;
+  this.category=category;
+}
+createbiscuits.prototype.display=function(){  
+  console.log("Name: " + this.name);
+  console.log("Price: " + this.price);
+  console.log("Quantity: " + this.qty);
+  console.log("Company: " + this.company);
+  console.log("Category: " + this.category);
 }
 
-// create toaster
-const toaster = createToaster({
-  positionX: "right",
-  positionY: "top",
-  theme: "dark",
-  duration: 3
-});
+let biscuit1 = new createbiscuits("parleg", 10, 100, "parleg", "cream");
 
-// test multiple
-toaster("Toast 1 🚀");
-setTimeout(() => toaster("hello  🔥"), 500);
-setTimeout(() => toaster("aditya is gayyy 😏"), 1000);
+class Createpencil{
+  constructor(name, price, qty, company, category,color){
+    this.name=name;
+    this.price=price;
+    this.qty=qty;
+    this.company=company;
+    this.category=category;
+    this.color=color;
+    
+  }
+  write(text){
+    let h1=document.createElement("h1");
+    h1.textContent=text;
+    h1.style.color= this.color;
+    document.body.appendChild(h1);
+  }
+}
+
+let p1 = new Createpencil("natraj", 5, 50, "natraj", "color pencil", "red");
+
