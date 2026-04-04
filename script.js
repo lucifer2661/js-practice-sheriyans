6a34ebb1737ee686af95da3c51338af6b8ee1c60
lucifer2661 <aditya.hackerface@gmail.com>
2026-04-04 10:58:54 +0530

    wprked on js async

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-function createbiscuits(name, price, qty, company, category){
-  this.name=name;
-  this.price=price;
-  this.qty=qty;
-  this.company=company;
-  this.category=category;
-}
-createbiscuits.prototype.display=function(){  
-  console.log("Name: " + this.name);
-  console.log("Price: " + this.price);
-  console.log("Quantity: " + this.qty);
-  console.log("Company: " + this.company);
-  console.log("Category: " + this.category);
-}
+//koi bhi code js mein line by line chalts hai aur ye natural pattern hota hai ki code line by line chale but kabhi
+//kabi asie cases aate hai life mai jaha par aapka code wait krta hai and utni der mein agla code 
+//chal jaata hai
 
-let biscuit1 = new createbiscuits("parleg", 10, 100, "parleg", "cream");
+console.log("Hello World");
+setTimeout(() => {
+    console.log("This is setTimeout");
+}, 2000);
+console.log("This is the end of the code");
 
-class Createpencil{
-  constructor(name, price, qty, company, category,color){
-    this.name=name;
-    this.price=price;
-    this.qty=qty;
-    this.company=company;
-    this.category=category;
-    this.color=color;
-    
-  }
-  write(text){
-    let h1=document.createElement("h1");
-    h1.textContent=text;
-    h1.style.color= this.color;
-    document.body.appendChild(h1);
-  }
-}
-
-let p1 = new Createpencil("natraj", 5, 50, "natraj", "color pencil", "red");
 
