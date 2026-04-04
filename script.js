af4bd0f214913458051eda421a4edfc6a0b1e66c
lucifer2661 <aditya.hackerface@gmail.com>
2026-04-04 11:35:31 +0530

    call back hell

--- script.js
+++ script.js
@@ -7,5 +7,41 @@ setTimeout(() => {
     console.log("This is setTimeout");
 }, 2000);
 console.log("This is the end of the code");
+//sync aisa code jo line by line chale 
+//async aisa code jo line by line na chale aur wait kare kisi event ke hone ka ya kisi condition ke fulfill hone ka
+//call back pattern and  call back hell
 
 
+function kuchhderbaddchalungfa(fnc){
+  setTimeout(fnc,Math.floor(Math.random()*1000));
+}
+kuchhderbaddchalungfa((function(){
+  console.log("1");
+})); 
+
+//ek function ko agar app ek  aur function de rahe ho parameter mein to
+//vo parameter vala function hota hai call back;
+
+function profilelekaraao(username,cb) {
+  setTimeout(() => {
+    console.log(`profile fetched of ${username}`);
+    cb({_id: "123", username, age: 26, email: "onlypain@chutiya.com"});
+  }, 
+  1000);
+}
+function saarepostlekaraao(userid,cb) {
+  console.log(`fetching posts of ${userid}`);
+  setTimeout(() => {
+    console.log(`posts fetched of ${userid}`);
+    cb({_id: userid, posts: ["hey", "hello", "hi"]});
+  }, 1000);
+}
+
+
+profilelekaraao("john",function(data){
+  console.log(data);
+  saarepostlekaraao(data._id,function(posts){
+    console.log(posts);
+  })
+
+});
